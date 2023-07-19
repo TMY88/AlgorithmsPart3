5bfafd3ae14040785a1b240ea1e1429499579af5
Михаил <m-toka@mail.ru>
2023-07-19 19:46:54 +0300

    Initial commit

---
+++ src/main/java/pro/sky/algorithmspart3/AlgorithmsPart3Application.java
@@ -0,0 +1,14 @@
+package pro.sky.algorithmspart3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsPart3Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AlgorithmsPart3Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/algorithmspart3/AlgorithmsPart3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.algorithmspart3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsPart3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
